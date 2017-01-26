e7c752e35e41075f78ba4544377dc78db7e8efe4
RiditaNizam <riditan@gmail.com>
2017-01-25 21:45:57 -0500

    Initial commit

---
+++ src/com/company/Diff21.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Diff21 {
+    public int diff21(int n) {
+        if (n > 21) {
+            return (2 * (Math.abs(n - 21)));
+        } else {
+            return (Math.abs(n - 21));
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Diff21 diff21Object = new Diff21();
+    }
+}
